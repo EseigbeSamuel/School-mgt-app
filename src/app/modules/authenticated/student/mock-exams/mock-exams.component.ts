8c70a453075b07906e69101076b0d8055cbd73c5
richardpromise <techelsoft@gmail.com>
2025-05-18 10:15:57 +0100

    side bar configurations

---
+++ src/app/modules/authenticated/student/mock-exams/mock-exams.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-exams',
+  imports: [],
+  templateUrl: './mock-exams.component.html',
+  styleUrl: './mock-exams.component.css'
+})
+export class MockExamsComponent {
+
+}
