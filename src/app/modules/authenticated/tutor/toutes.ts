70296d566980c38184b6486aac9b50752d9b4b77
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-05-18 16:33:34 +0100

    test

--- src/app/modules/authenticated/tutor/toutes.ts
+++ src/app/modules/authenticated/tutor/toutes.ts
@@ -1,10 +1,15 @@
 // tutor/routes.ts
 import { Routes } from '@angular/router';
 import { TutorDashboardComponent } from './dashboard/dashboard.component';
+import { StudentsComponent } from './students/students.component';
 
 export const tutorRoutes: Routes = [
   {
     path: '',
     component: TutorDashboardComponent,
+  },
+  {
+    path: 'students',
+    component: StudentsComponent,
   },
 ];
