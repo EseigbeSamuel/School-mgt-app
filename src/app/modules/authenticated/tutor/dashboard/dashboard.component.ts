511db7de7073a1fdd8a10d7aac24cb98df7c03a2
richardpromise <richardpromise013@gmail.com>
2025-05-19 20:49:53 +0100

    made update

--- src/app/modules/authenticated/tutor/dashboard/dashboard.component.ts
+++ src/app/modules/authenticated/tutor/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../shared/shared.module';
 import { CurrencyFormatPipe } from '../../../../pipe/currency-format.pipe';
-
+import { DomSanitizer } from '@angular/platform-browser';
+import { stats as NAV_LINKS } from './data';
+import { CommonModule } from '@angular/common';
+import { DasboardCardSvgIcons } from '../../../../utils/icons';
 @Component({
   selector: 'app-dashboard',
   imports: [SharedModule, CurrencyFormatPipe],
