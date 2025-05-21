d9f753a5674c45311602ceeac2c4b0df8a0b3381
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-05-21 20:06:17 +0100

    profile page partialy built

--- src/app/modules/authenticated/student/personal-sessions/personal-sessions.component.ts
+++ src/app/modules/authenticated/student/personal-sessions/personal-sessions.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { InputUiComponent } from '../../../../shared/components/input-ui/input-ui.component';
+import { SharedModule } from '../../../../shared/shared.module';
 
 @Component({
   selector: 'app-personal-sessions',
-  imports: [],
+  imports: [InputUiComponent, SharedModule],
   templateUrl: './personal-sessions.component.html',
-  styleUrl: './personal-sessions.component.css'
+  styleUrl: './personal-sessions.component.css',
 })
-export class PersonalSessionsComponent {
-
-}
+export class PersonalSessionsComponent {}
