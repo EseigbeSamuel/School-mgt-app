72d11fe543775fb7ee6b1eaf765641c234abaf08
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-05-28 19:37:29 +0100

    quiz unfinished

--- src/app/modules/authenticated/student/assessment-and-quizzes/quiz/quiz.component.ts
+++ src/app/modules/authenticated/student/assessment-and-quizzes/quiz/quiz.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../../shared/shared.module';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
-  imports: [SharedModule],
+  imports: [SharedModule, NgIf],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css',
 })
-export class QuizComponent {}
+export class QuizComponent {
+  invisible = false;
+}
