285814a9f1341be05885bdf4d836d63b9acfdcf3
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-06-04 18:38:27 +0100

    updated profile page

--- src/app/modules/authenticated/student/profile/main-profile/main-profile.component.ts
+++ src/app/modules/authenticated/student/profile/main-profile/main-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../../shared/shared.module';
-import { NgClass, NgForOf, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { InputUiComponent } from '../../../../../shared/components/input-ui/input-ui.component';
 import { IntlPhoneInputComponent } from '../../../../../shared/components/intl-phone-input/intl-phone-input.component';
 import { RouterLink } from '@angular/router';
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
   imports: [
     SharedModule,
     NgClass,
-    NgForOf,
+    NgFor,
     InputUiComponent,
     IntlPhoneInputComponent,
     NgIf,
@@ -20,36 +20,92 @@ import { RouterLink } from '@angular/router';
   styleUrl: './main-profile.component.css',
 })
 export class MainProfileComponent {
-  lessons = [
+  tasks = [
     {
-      subject: 'Biology',
-      category: 'JAMB/UMBE CATEGORY',
-      started: '02/10/2025',
-      progress: 80,
-      color: 'bg-blue-500',
+      task: 'English Tutor',
+      class: 'Group class',
+      date: 'May 12, 2025',
+      time: '09.00 - 10.00 AM',
     },
     {
-      subject: 'Physics',
-      category: 'WAEC/NECO CATEGORY',
-      started: '02/11/2025',
-      progress: 20,
-      color: 'bg-red-500',
+      task: 'English Tutor',
+      class: 'Group class',
+      date: 'May 12, 2025',
+      time: '09.00 - 10.00 AM',
     },
     {
-      subject: 'Mathematics',
-      category: 'WAEC/NECO CATEGORY',
-      started: '03/04/2025',
-      progress: 60,
-      color: 'bg-orange-500',
-    },
-    {
-      subject: 'Chemistry',
-      category: 'JAMB/UMBE CATEGORY',
-      started: '03/11/2025',
-      progress: 20,
-      color: 'bg-red-500',
+      task: 'English Tutor',
+      class: 'Group class',
+      date: 'May 12, 2025',
+      time: '09.00 - 10.00 AM',
     },
   ];
+  progress = [
+    {
+      topic: 'Introductory to prefix and suffix',
+      subject: 'Use of English',
+      progress: 60,
+    },
+    {
+      topic: 'Introductory to prefix and suffix',
+      subject: 'Use of English',
+      progress: 60,
+    },
+    {
+      topic: 'Introductory to prefix and suffix',
+      subject: 'Use of English',
+      progress: 60,
+    },
+  ];
+
+  columns = [
+    { key: 'transactionId', header: 'Transaction ID' },
+    { key: 'date', header: 'Date' },
+    { key: 'amount', header: 'Amount' },
+    { key: 'status', header: 'Status' },
+    { key: 'action', header: 'Action' },
+  ];
+
+  data: { [key: string]: any }[] = [
+    {
+      transactionId: '#123456789',
+      date: '2 March 2021',
+      amount: 'NGN 30,000',
+      status: 'Confirmed',
+    },
+    {
+      transactionId: '#123456789',
+      date: '2 March 2021',
+      amount: 'NGN 30,000',
+      status: 'Canceled',
+    },
+    {
+      transactionId: '#123456789',
+      date: '2 March 2021',
+      amount: 'NGN 30,000',
+      status: 'Pending',
+    },
+    {
+      transactionId: '#123456789',
+      date: '2 March 2021',
+      amount: 'NGN 30,000',
+      status: 'Complete',
+    },
+  ];
+
+  getStatusClass(status: string): string {
+    switch (status.toLowerCase()) {
+      case 'confirmed':
+      case 'complete':
+        return 'bg-[#05CD991A] text-[#05CD99]';
+      case 'canceled':
+        return 'bg-[#F90B0B1A] text-[#F90B0B]';
+      case 'pending':
+        return 'bg-[#CDC6051A] text-[#CDC605] ';
+      default:
+        return 'bg-gray-100 text-gray-600';
+    }
+  }
 
   invisible = false;
   deleteAccount = false;
