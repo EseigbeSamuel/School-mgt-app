3bc78652d9d2ec69167c1b7e4f313ea758ec25ec
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-06-06 15:09:30 +0100

    message page in progress

--- src/app/modules/authenticated/student/messages/chat-side/chat-side.component.ts
+++ src/app/modules/authenticated/student/messages/chat-side/chat-side.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FriendsDataService } from '../../../../../services/friends-data.service';
+import { Location, NgIf } from '@angular/common';
+import { SharedModule } from '../../../../../shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat-side',
-  imports: [],
+  imports: [NgIf, SharedModule],
   templateUrl: './chat-side.component.html',
-  styleUrl: './chat-side.component.css'
+  styleUrl: './chat-side.component.css',
 })
-export class ChatSideComponent {
+export class ChatSideComponent implements OnInit {
+  friends: any;
+  constructor(
+    private friendsDataService: FriendsDataService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
+  // ngOnInit(): void {
+  //   this.friends = this.friendsDataService.getFriends();
+  // }
+
+  goBack() {
+    this.location.back();
+  }
+  ngOnInit() {
+    this.friendsDataService.selectedFriends$.subscribe((friends) => {
+      this.friends = friends;
+    });
+  }
 }
