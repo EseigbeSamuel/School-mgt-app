1e491ce7d5762303864ad4c2fd00d4e4f4d30ef8
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-06-07 20:35:01 +0100

    mock-test page pending

---
+++ src/app/modules/authenticated/student/achievements/streaks/streaks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-streaks',
+  imports: [],
+  templateUrl: './streaks.component.html',
+  styleUrl: './streaks.component.css'
+})
+export class StreaksComponent {
+
+}
