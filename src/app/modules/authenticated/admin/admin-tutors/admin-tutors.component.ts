b8a179bfeb78672e5a8858478e39652a1bc85392
dalgoridim <b.fadamitan2019@gmail.com>
2025-06-14 20:06:43 +0100

    - admin components

---
+++ src/app/modules/authenticated/admin/admin-tutors/admin-tutors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tutors',
+  imports: [],
+  templateUrl: './admin-tutors.component.html',
+  styleUrl: './admin-tutors.component.css'
+})
+export class AdminTutorsComponent {
+
+}
