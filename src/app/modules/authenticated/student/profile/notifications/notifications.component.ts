93e0670bc9f604aa7a4a9432df1b73df03aecee7
EseigbeSamuel <eseigbesamuel810@gmail.com>
2025-06-22 20:11:37 +0100

    minor ui fixes

--- src/app/modules/authenticated/student/profile/notifications/notifications.component.ts
+++ src/app/modules/authenticated/student/profile/notifications/notifications.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SharedModule } from '../../../../../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notifications',
-  imports: [RouterLink],
+  imports: [RouterLink, SharedModule, CommonModule, FormsModule],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css',
 })
-export class NotificationsComponent {}
+export class NotificationsComponent {
+  toggleAll = false;
+
+  notificationOptions = [
+    { label: 'Phone notifications', checked: true },
+    { label: 'Email push notifications', checked: true },
+    { label: 'Subscribe to newsletter', checked: true },
+    { label: 'New update from FlexyDemy', checked: false },
+    { label: 'I want to know who viewed my course.', checked: true },
+    {
+      label: 'I want to know who write a review on my course.',
+      checked: false,
+    },
+    { label: 'I want to know who commented on my lecture.', checked: true },
+    { label: 'I want to know who download my lecture notes.', checked: false },
+    { label: 'I want to know who replied on my comment.', checked: false },
+    {
+      label: 'I want to know daily how many people visited my profile.',
+      checked: true,
+    },
+    {
+      label: 'I want to know who download my lecture attach file.',
+      checked: false,
+    },
+  ];
+
+  toggleAllNotifications(): void {
+    this.notificationOptions.forEach((option) => {
+      option.checked = this.toggleAll;
+    });
+  }
+}
